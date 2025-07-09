98232d042b708566cd377e9e8b5d48396db392af
basdidon <39435008+basdidon@users.noreply.github.com>
2025-07-09 19:27:46 +0700

    added payment service

---
+++ payment.api/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
+StripeConfiguration.ApiKey = "";
+
+var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapGet("/", () => "Hello World!");
+app.MapPost("/create-checkout-session", () =>
+{
+    var domain = "http://localhost:3000";
+    var options = new SessionCreateOptions
+    {
+        UiMode = "custom",
+        LineItems =
+        [
+            new SessionLineItemOptions
+            {
+                // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+                //Price = "{{PRICE_ID}}",
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    Currency = "usd",
+                    UnitAmount = 5000, // $50.00 (in cents)
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = "Custom T-Shirt"
+                    }
+                },
+                Quantity = 1,
+            },
+        ],
+        Mode = "payment",
+        PaymentMethodTypes = ["card","klarna"],
+        ReturnUrl = domain + "/return?session_id={CHECKOUT_SESSION_ID}",
+    };
+    var service = new SessionService();
+    Session session = service.Create(options);
+
+    return Results.Json(new { clientSecret = session.ClientSecret });
+});
+
+app.MapGet("/session-status", ([FromQuery] string session_id) =>
+{
+    var sessionService = new SessionService();
+    Session session = sessionService.Get(session_id);
+
+    return Results.Json(new { status = session.Status, customer_email = session.CustomerDetails.Email });
+});
+app.Run();
